d3e045d8b5432960961c992d9f2e0c9e81504a29
William Burton <william.burton@welocalize.com>
2016-09-28 17:18:45 -0400

    removed whitenoise storage

--- wevoice/settings.py
+++ wevoice/settings.py
@@ -141,10 +141,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 2000
 
 LOGIN_URL = '/'
