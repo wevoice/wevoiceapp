827819052826d9b73c27e321f87bf21f6a8ddc68
William Burton <william.burton@welocalize.com>
2016-09-28 17:05:27 -0400

    reverted wsgi.py to standard

--- wevoice/wsgi.py
+++ wevoice/wsgi.py
@@ -4,14 +4,13 @@ WSGI config for wevoice project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wevoice.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wevoice.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
