c6c156b132164576d94361681c3b39833dc904ef
William Burton <william.burton@welocalize.com>
2016-11-02 14:58:35 -0400

    initial contextual filters

--- choices/admin.py
+++ choices/admin.py
@@ -9,6 +9,282 @@ from django.forms import Textarea
 from django.db import models as dbmodels
 from django.db.models import Count, Q
 from choices.models import Talent
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import force_text, smart_text
+from django.contrib.admin.utils import reverse_field_path
+
+# FILTERS #
+
+
+def filter_lookups_queryset(request, qs, parameter_name=None):
+    target_params = ('gender__exact', 'type__exact', 'age_range__exact', 'average_rating', 'vendor__id__exact',
+                     'language__id__exact')
+    initial_attrs = dict([(param, val) for param, val in request.GET.iteritems() if param in target_params and val])
+    qs = qs.filter(**initial_attrs)
+    status_params = ('preapproved', 'approved', 'rejected')
+    if parameter_name:
+        status_attrs = dict([('selection__status', parameter_name.upper())])
+        qs = qs.filter(**status_attrs).annotate(obj_count=Count('pk'))
+        extra_status_attrs = [(param, val) for param, val in request.GET.iteritems()
+                              if param in status_params and val and param != parameter_name]
+    else:
+        extra_status_attrs = [(param, val) for param, val in request.GET.iteritems()
+                              if param in status_params and val]
+    if len(extra_status_attrs) > 0:
+        for item in extra_status_attrs:
+            qs = qs.filter(id__in=Talent.objects
+                           .filter(selection__status=item[0].upper())
+                           .annotate(status_count=Count('selection__status'))
+                           .filter(status_count=int(int(item[1]))))
+    return qs
+
+
+class InReviewFilter(admin.SimpleListFilter):
+    parameter_name = 'under_review'
+    title = 'in review'
+    YES, NO = 1, 0
+
+    def lookups(self, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        queryset = filter_lookups_queryset(request, queryset)
+
+        yes_test = queryset.filter(
+            Q(selection__status="REJECTED") |
+            Q(selection__status="APPROVED") |
+            Q(selection__status="PREAPPROVED")
+        ).distinct()
+
+        no_test = queryset.exclude(
+            Q(selection__status="REJECTED") |
+            Q(selection__status="APPROVED") |
+            Q(selection__status="PREAPPROVED")
+        ).distinct()
+
+        prompts = ()
+
+        if yes_test.count() > 0:
+            prompts += ((self.YES, 'yes'),)
+
+        if no_test.count() > 0:
+            prompts += ((self.NO, 'no'),)
+
+        return prompts
+
+    def queryset(self, request, queryset):
+        queryset = filter_lookups_queryset(request, queryset)
+        if self.value() and int(self.value()) == self.YES:
+            return queryset.filter(
+                Q(selection__status="REJECTED") |
+                Q(selection__status="APPROVED") |
+                Q(selection__status="PREAPPROVED")
+            ).distinct()
+        if self.value() and int(self.value()) == self.NO:
+            return queryset.exclude(
+                Q(selection__status="REJECTED") |
+                Q(selection__status="APPROVED") |
+                Q(selection__status="PREAPPROVED")
+            ).distinct()
+        return queryset
+
+
+class StatusFilter(admin.SimpleListFilter):
+    title = ''
+    parameter_name = ''
+
+    def lookups(self, request, model_admin):
+        qs = filter_lookups_queryset(request, model_admin.get_queryset(request), self.parameter_name)
+        tuple_set = ()
+        for number in sorted(qs.order_by().values_list('obj_count', flat=True).distinct()):
+            tuple_set += ((number, str(number)),)
+        return tuple_set
+
+    def queryset(self, request, queryset):
+        if self.value():
+            qs = self.process_queryset(request, queryset)
+            return qs
+        else:
+            return queryset
+
+    def process_queryset(self, request, queryset):
+        if hasattr(queryset[0], 'status_count'):
+            qs = queryset.filter(id__in=Talent.objects
+                                 .filter(selection__status=self.parameter_name.upper())
+                                 .annotate(status_count=Count('selection__status'))
+                                 .filter(status_count=int(request.GET[self.parameter_name])))
+        else:
+            qs = queryset.filter(selection__status=self.parameter_name.upper()).annotate(status_count=Count('pk'))
+            qs = qs.filter(status_count=int(request.GET[self.parameter_name]))
+        return qs
+
+
+class PreapprovedFilter(StatusFilter):
+    title = 'times preapproved'
+    parameter_name = 'preapproved'
+
+
+class ApprovedFilter(StatusFilter):
+    title = 'times accepted'
+    parameter_name = 'approved'
+
+
+class RejectedFilter(StatusFilter):
+    title = 'times rejected'
+    parameter_name = 'rejected'
+
+
+class FilteredChoicesFieldListFilter(admin.FieldListFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg = '%s__exact' % field_path
+        self.lookup_kwarg_isnull = '%s__isnull' % field_path
+        self.lookup_val = request.GET.get(self.lookup_kwarg)
+        self.lookup_val_isnull = request.GET.get(self.lookup_kwarg_isnull)
+        queryset = model_admin.get_queryset(request)
+        # Obey parent ModelAdmin queryset when deciding which options to show
+        parent_model, reverse_path = reverse_field_path(model, field_path)
+        if model == parent_model:
+            queryset = model_admin.get_queryset(request)
+        else:
+            queryset = parent_model._default_manager.all()
+        queryset = filter_lookups_queryset(request, queryset)
+        self.lookup_choices = (queryset
+                               .distinct()
+                               .order_by(field.name)
+                               .values_list(field.name, flat=True))
+
+        super(FilteredChoicesFieldListFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
+
+    def expected_parameters(self):
+        return [self.lookup_kwarg, self.lookup_kwarg_isnull]
+
+    def choices(self, changelist):
+        yield {
+            'selected': self.lookup_val is None,
+            'query_string': changelist.get_query_string(
+                {}, [self.lookup_kwarg, self.lookup_kwarg_isnull]
+            ),
+            'display': _('All')
+        }
+        none_title = ''
+        for lookup, title in self.field.flatchoices:
+            if lookup is None:
+                none_title = title
+                continue
+            if lookup in self.lookup_choices:
+                yield {
+                    'selected': smart_text(lookup) == self.lookup_val,
+                    'query_string': changelist.get_query_string(
+                        {self.lookup_kwarg: lookup}, [self.lookup_kwarg_isnull]
+                    ),
+                    'display': title,
+                }
+        if none_title:
+            yield {
+                'selected': bool(self.lookup_val_isnull),
+                'query_string': changelist.get_query_string({
+                    self.lookup_kwarg_isnull: 'True',
+                }, [self.lookup_kwarg]),
+                'display': none_title,
+            }
+admin.FieldListFilter.register(lambda f: bool(f.choices), FilteredChoicesFieldListFilter)
+
+
+class FilteredAllValuesFieldListFilter(admin.FieldListFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg = field_path
+        self.lookup_kwarg_isnull = '%s__isnull' % field_path
+        self.lookup_val = request.GET.get(self.lookup_kwarg)
+        self.lookup_val_isnull = request.GET.get(self.lookup_kwarg_isnull)
+        self.empty_value_display = model_admin.get_empty_value_display()
+        parent_model, reverse_path = reverse_field_path(model, field_path)
+        # Obey parent ModelAdmin queryset when deciding which options to show
+        if model == parent_model:
+            queryset = model_admin.get_queryset(request)
+        else:
+            queryset = parent_model._default_manager.all()
+        queryset = filter_lookups_queryset(request, queryset)
+        self.lookup_choices = (queryset
+                               .distinct()
+                               .order_by(field.name)
+                               .values_list(field.name, flat=True))
+        super(FilteredAllValuesFieldListFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
+
+    def expected_parameters(self):
+        return [self.lookup_kwarg, self.lookup_kwarg_isnull]
+
+    def choices(self, changelist):
+        yield {
+            'selected': self.lookup_val is None and self.lookup_val_isnull is None,
+            'query_string': changelist.get_query_string({}, [self.lookup_kwarg, self.lookup_kwarg_isnull]),
+            'display': _('All'),
+        }
+        include_none = False
+        for val in self.lookup_choices:
+            if val is None:
+                include_none = True
+                continue
+            val = smart_text(val)
+            yield {
+                'selected': self.lookup_val == val,
+                'query_string': changelist.get_query_string({
+                    self.lookup_kwarg: val,
+                }, [self.lookup_kwarg_isnull]),
+                'display': val,
+            }
+        if include_none:
+            yield {
+                'selected': bool(self.lookup_val_isnull),
+                'query_string': changelist.get_query_string({
+                    self.lookup_kwarg_isnull: 'True',
+                }, [self.lookup_kwarg]),
+                'display': self.empty_value_display,
+            }
+admin.FieldListFilter.register(lambda f: True, FilteredAllValuesFieldListFilter)
+
+
+class FilteredRelatedOnlyFieldListFilter(admin.RelatedFieldListFilter):
+    def field_choices(self, field, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        queryset = filter_lookups_queryset(request, queryset)
+        pk_qs = queryset.distinct().values_list('%s__pk' % self.field_path, flat=True)
+        return field.get_choices(include_blank=False, limit_choices_to={'pk__in': pk_qs})
+
+
+class CommentsCountFilter(admin.SimpleListFilter):
+    parameter_name = 'is_commented'
+    title = 'Has Comments'
+    YES, NO = 1, 0
+
+    def lookups(self, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        queryset = filter_lookups_queryset(request, queryset)
+        queryset = queryset.annotate(comment_count=Count('comments'))
+
+        yes_test = queryset.filter(comment_count__gte=1)
+        no_test = queryset.filter(comment_count__lt=1)
+
+        prompts = ()
+        if yes_test.count() > 0:
+            prompts += ((self.YES, 'yes'),)
+
+        if no_test.count() > 0:
+            prompts += ((self.NO, 'no'),)
+
+        return prompts
+
+    def queryset(self, request, queryset):
+        queryset = filter_lookups_queryset(request, queryset)
+        qs = queryset.annotate(comment_count=Count('comments'))
+
+        if self.value() and int(self.value()) == self.YES:
+            return qs.filter(comment_count__gte=1)
+        if self.value() and int(self.value()) == self.NO:
+            return qs.filter(comment_count__lt=1)
+
+        return queryset
+
+# END FILTERS #
 
 
 class UserProfileInline(admin.StackedInline):
@@ -84,100 +360,10 @@ class RatingAdmin(admin.ModelAdmin):
 admin.site.register(models.Rating, RatingAdmin)
 
 
-class InReviewFilter(admin.SimpleListFilter):
-    parameter_name = 'under_review'
-    title = 'in review'
-    YES, NO = 1, 0
-
-    def lookups(self, request, model_admin):
-        return (
-            (self.YES, 'yes'),
-            (self.NO, 'no'),
-        )
-
-    def queryset(self, request, queryset):
-        if self.value() and int(self.value()) == self.YES:
-            return queryset.filter(
-                Q(selection__status="REJECTED") |
-                Q(selection__status="APPROVED") |
-                Q(selection__status="PREAPPROVED")
-            ).distinct()
-        if self.value() and int(self.value()) == self.NO:
-            return queryset.exclude(
-                Q(selection__status="REJECTED") |
-                Q(selection__status="APPROVED") |
-                Q(selection__status="PREAPPROVED")
-            ).distinct()
-        return queryset
-
-
 class RatingInline(admin.StackedInline):
     model = models.Rating
     readonly_fields = ('rater', 'talent', 'rating')
     extra = 0
-
-
-class StatusFilter(admin.SimpleListFilter):
-    title = ''
-    parameter_name = ''
-
-    def filter_lookups_queryset(self, request, qs):
-        target_params = ('gender__exact', 'type__exact', 'age_range__exact', 'average_rating', 'vendor__id__exact',
-                         'language__id__exact')
-        initial_attrs = dict([(param, val) for param, val in request.GET.iteritems() if param in target_params and val])
-        qs = qs.filter(**initial_attrs)
-        status_attrs = dict([('selection__status', self.parameter_name.upper())])
-        qs = qs.filter(**status_attrs).annotate(obj_count=Count('pk'))
-        status_params = ('preapproved', 'approved', 'rejected')
-        extra_status_attrs = [(param, val) for param, val in request.GET.iteritems()
-                              if param in status_params and val and param != self.parameter_name]
-        if len(extra_status_attrs) > 0:
-            for item in extra_status_attrs:
-                qs = qs.filter(id__in=Talent.objects
-                               .filter(selection__status=item[0].upper())
-                               .annotate(status_count=Count('selection__status'))
-                               .filter(status_count=int(int(item[1]))))
-        return qs
-
-    def lookups(self, request, model_admin):
-        qs = self.filter_lookups_queryset(request, model_admin.get_queryset(request))
-        tuple_set = ()
-        for number in sorted(qs.order_by().values_list('obj_count', flat=True).distinct()):
-            tuple_set += ((number, str(number)),)
-        return tuple_set
-
-    def queryset(self, request, queryset):
-        if self.value():
-            qs = self.process_queryset(request, queryset)
-            return qs
-        else:
-            return queryset
-
-    def process_queryset(self, request, queryset):
-        if hasattr(queryset[0], 'status_count'):
-            qs = queryset.filter(id__in=Talent.objects
-                                 .filter(selection__status=self.parameter_name.upper())
-                                 .annotate(status_count=Count('selection__status'))
-                                 .filter(status_count=int(request.GET[self.parameter_name])))
-        else:
-            qs = queryset.filter(selection__status=self.parameter_name.upper()).annotate(status_count=Count('pk'))
-            qs = qs.filter(status_count=int(request.GET[self.parameter_name]))
-        return qs
-
-
-class PreapprovedFilter(StatusFilter):
-    title = 'times preapproved'
-    parameter_name = 'preapproved'
-
-
-class ApprovedFilter(StatusFilter):
-    title = 'times accepted'
-    parameter_name = 'approved'
-
-
-class RejectedFilter(StatusFilter):
-    title = 'times rejected'
-    parameter_name = 'rejected'
 
 
 class TalentAdmin(admin.ModelAdmin):
@@ -193,16 +379,16 @@ class TalentAdmin(admin.ModelAdmin):
     formfield_overrides = {
         dbmodels.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 50})},
     }
-    list_filter = ('gender',
-                   'type',
-                   'age_range',
-                   'average_rating',
+    list_filter = (('gender', FilteredChoicesFieldListFilter),
+                   ('type', FilteredChoicesFieldListFilter),
+                   ('age_range', FilteredChoicesFieldListFilter),
+                   ('average_rating', FilteredAllValuesFieldListFilter),
                    InReviewFilter,
                    PreapprovedFilter,
                    ApprovedFilter,
                    RejectedFilter,
-                   ('vendor', admin.RelatedOnlyFieldListFilter),
-                   ('language', admin.RelatedOnlyFieldListFilter))
+                   ('vendor', FilteredRelatedOnlyFieldListFilter),
+                   ('language', FilteredRelatedOnlyFieldListFilter))
     list_display = ('id', 'welo_id', 'vendor', 'audio_file_player', 'language', 'type', 'gender', 'age_range',
                     'average_rating', 'get_times_preapproved', 'get_times_accepted', 'get_times_rejected')
     list_display_links = ('id', 'welo_id')
@@ -230,29 +416,6 @@ class TalentAdmin(admin.ModelAdmin):
 admin.site.register(models.Talent, TalentAdmin)
 
 
-class CommentsCountFilter(admin.SimpleListFilter):
-    parameter_name = 'is_commented'
-    title = 'Has Comments'
-    YES, NO = 1, 0
-    THRESHOLD = 1
-
-    def lookups(self, request, model_admin):
-        return (
-            (self.YES, 'yes'),
-            (self.NO, 'no'),
-        )
-
-    def queryset(self, request, queryset):
-        qs = queryset.annotate(Count('comments'))
-
-        if self.value() and int(self.value()) == self.YES:
-            return qs.filter(comments__count__gte=self.THRESHOLD)
-        if self.value() and int(self.value()) == self.NO:
-            return qs.filter(comments__count__lt=self.THRESHOLD)
-
-        return queryset
-
-
 class CommentInline(admin.StackedInline):
     model = models.Comment
     readonly_fields = ('selection', 'author', 'text', 'created_date')
@@ -261,14 +424,15 @@ class CommentInline(admin.StackedInline):
 
 class SelectionAdmin(admin.ModelAdmin):
     inlines = [CommentInline]
-    list_filter = ('status',
-                   'talent__gender',
-                   CommentsCountFilter,
-                   'talent__average_rating',
-                   ('client', admin.RelatedOnlyFieldListFilter),
-                   ('talent__vendor', admin.RelatedOnlyFieldListFilter),
-                   ('talent__language', admin.RelatedOnlyFieldListFilter)
-                   )
+    list_filter = (
+        'status',
+        'talent__gender',
+        CommentsCountFilter,
+        ('talent__average_rating', FilteredAllValuesFieldListFilter),
+        ('client', FilteredRelatedOnlyFieldListFilter),
+        ('talent__vendor', FilteredRelatedOnlyFieldListFilter),
+        ('talent__language', FilteredRelatedOnlyFieldListFilter)
+    )
     list_display = ('id', 'talent', 'client', 'status', 'audio_file_player', 'talent_language',
                     'talent_gender', 'talent_vendor', 'talent_age_range', 'talent_average_rating', 'total_comments')
     search_fields = ['client__username', 'client__name', 'talent__welo_id', 'talent__vendor__name']
